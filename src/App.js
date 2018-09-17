1ddf8a4b2730b6aec045af7e08903bf11c7ddfcb
Tobias Skjelvik <trampcha@gmail.com>
2018-09-17 18:18:31 +0200

    Lagt til filene for Tab og Tabs, samt noe placeholder CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import RadioButtonContainer from './components/RadioButtonContainer';
+import Tabs from './components/Tabs';
 {/*import CheckboxContainer from './components/CheckboxContainer';*/}
 
 class App extends Component {
@@ -9,7 +10,6 @@ class App extends Component {
     return (
       <div>
         <h1>Tabs Demo</h1>
-
           <RadioButtonContainer>
               <div buttonValue={1} label={"hali"}/>
               <div buttonValue={2} label={"og"}/>
@@ -18,6 +18,11 @@ class App extends Component {
               <div buttonValue={5} label={"hælvette"}/>
               <div buttonValue={6} label={"kuk"}/>
           </RadioButtonContainer>
+          <Tabs>
+            <div tabName="Gator">See ya later, <em>Alligator</em>!</div>
+            <div tabName="Croc">After a while, <em>Crocodile</em>!</div>
+            <div tabName="Sarcosuchus">Nothing to see here, this tab is <em>extinct</em>!</div>
+          </Tabs>
 
 
       </div>
