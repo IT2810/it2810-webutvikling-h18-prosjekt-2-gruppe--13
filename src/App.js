2869999cecb810cc6c17b176486a7caee8606c6a
Magnus Rand <magnus.rand@gmail.com>
2018-09-18 10:46:17 +0200

    Added som comments #2 #1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import RadioButtonContainer from './components/RadioButtonContainer';
+import ImageFile from "./components/ImageFile";
 {/*import CheckboxContainer from './components/CheckboxContainer';*/}
 
 class App extends Component {
@@ -10,14 +11,7 @@ class App extends Component {
       <div>
         <h1>Tabs Demo</h1>
 
-          <RadioButtonContainer>
-              <div buttonValue={1} label={"hali"}/>
-              <div buttonValue={2} label={"og"}/>
-              <div buttonValue={3} label={"dra"}/>
-              <div buttonValue={4} label={"til"}/>
-              <div buttonValue={5} label={"hælvette"}/>
-              <div buttonValue={6} label={"kuk"}/>
-          </RadioButtonContainer>
+          <ImageFile src={'public/resources/images.json'} albumId={1}/>
 
 
       </div>
--- src/components/RadioButtonContainer.js
+++ src/components/RadioButtonContainer.js
@@ -1,33 +1,34 @@
 import React from 'react';
+import RadioButton from './RadioButton';
 
 
 export default class RadioButtonContainer extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleChange.bind(this); // Binds 'this' correctly. Not sure if necessary
         this.handleSubmit = this.handleSubmit.bind(this);
 
         this.state = {
-            selectedId: 2,
+            selectedId: 2, // Indicates which button should be selected on load (blank for none)
         };
 
     }
 
-    handleChange = (i) => {
+    handleChange = (i) => { // Should be run when user clicks on a radioButton. Changes state to correct selectedId
         this.setState(
             {selectedId: i}
         );
         console.log(this.state.selectedId);
     };
 
-    handleSubmit = (event) => {
+    handleSubmit = (event) => { // Not in use (yet?)
         event.preventDefault();
 
         alert('Du valgte knapp med id: ' + this.state.selectedId.text());
     };
 
-    renderButton = (i, key, label) => {
+    renderButton = (i, key, label) => { // Function for rendering a radioButton, takes inn 'id', 'key' which is the index in the list, and a label
         return (
             <RadioButton
                 id={i}
@@ -39,13 +40,13 @@ export default class RadioButtonContainer extends React.Component {
         );
     };
 
-    render() {
+    render() { // renders the form containing a list of RadioButton-components. The number of components is determined by the number of children the RadioButtonContainer has
         return (
             <form onSubmit={this.handleSubmit}>
                 <p> Select a button</p>
                 <ul className="Radiobuttons">
                     {
-                       this.props.children.map((child, index) => {
+                       this.props.children.map((child, index) => { // Maps each child to a rendered RadioButton-component
                            const {buttonValue, label} = child.props;
                            return (
                                this.renderButton(buttonValue, index, label)
@@ -56,21 +57,4 @@ export default class RadioButtonContainer extends React.Component {
             </form>
         )
     }
-}
-
-function RadioButton(props) {
-
-    return (
-        <li>
-            <label>
-                <input
-                    type="radio"
-                    value={props.id}
-                    checked={props.selectedId === props.id}
-                    onChange={props.onChange}
-                />
-                {props.label}
-            </label>
-        </li>
-    )
 }
